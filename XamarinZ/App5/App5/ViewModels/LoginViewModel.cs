50e675c318621c6bfa21e7768ec2674155cdef29
Your Name <you@example.com>
2019-05-20 13:35:05 -0400

    Xamarin terminado 11:12

---
+++ XamarinZ/App5/App5/ViewModels/LoginViewModel.cs
@@ -0,0 +1,48 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace App5.ViewModels
+{
+    class LoginViewModel : BaseViewModel
+    {
+        #region variables
+        string email;
+        string password;
+        bool isrunning;
+        #endregion
+        #region Propiedades
+        public string Email
+        {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+
+        }
+        public bool IsRunning
+        {
+            get { return this.isrunning; }
+            set { SetValue(ref this.isrunning, value); }
+        }
+
+        #endregion
+
+        #region Comandos
+        public ICommand LoginCommand
+        {
+            get { return new RelayCommand(Login); }
+        }
+        private void Login()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+
+}
